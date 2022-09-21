5a7c53805d02eea33a41c4d67448ae49ba1e555f
Willem Ruys <willem@founda.com>
2022-09-21 17:12:39 +0200

    initial changes

--- egret.py
+++ egret.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
 import re
 import sys	
 import egret_ext
@@ -90,149 +91,113 @@ if opts.fileName != None and opts.regex != None:
     sys.exit(-1)
 
 # get the regular expression
-descStr = ""
+output = []
+descStr = []
+regexStrings = []
 if opts.fileName != None:
     inFile = open(opts.fileName)
-    regexStr = inFile.readline().rstrip()
-    try:
-        descStr = inFile.readline().rstrip()
-    except:
-        descStr = ""
-    inFile.close()
-elif opts.regex != None:
-    regexStr = opts.regex
-else:
-    regexStr = input("Enter a Regular Expression: ")
 
+    fileAsJson = json.load(inFile)
+    for regexObject in fileAsJson:
+        regexStrings.append(regexObject['pattern'])
+        try:
+            descStr.append(regexObject['pattern']) 
+        except:
+            descStr.append("") 
+        inFile.close()
 # compile the regular expression
-try:
-    regex = re.compile(regexStr)
 
-    # execute regex-test
-    #start_time = time.process_time()
-    inputStrs = egret_ext.run(regexStr, opts.baseSubstring,
-        False, False, opts.debugMode, opts.statMode)
-    status = inputStrs[0]
-    hasError = (status[0:5] == "ERROR")
+for regexStr in regexStrings:
+    try:
+        regex = re.compile(regexStr)
 
-except re.error as e:
-    status = "ERROR (compiler error): Regular expression did not compile: " + str(e)
-    hasError = True
+        # execute regex-test
+        #start_time = time.process_time()
+        inputStrs = egret_ext.run(regexStr, opts.baseSubstring,
+            False, False, opts.debugMode, opts.statMode)
+        status = inputStrs[0]
+        hasError = (status[0:5] == "ERROR")
 
-if hasError:
-    alerts = [status]
-else:
-    idx = 0
-    line = inputStrs[idx]
-    while line != "BEGIN":
-        idx += 1;
+    except re.error as e:
+        status = "ERROR (compiler error): Regular expression did not compile: " + str(e)
+        hasError = True
+
+    if hasError:
+        alerts = [status]
+    else:
+        idx = 0
         line = inputStrs[idx]
-    if idx == 0:
-        alerts = []
-        inputStrs = inputStrs[1:]
-    else:
-        alerts = inputStrs[:idx]
-        inputStrs = inputStrs[idx+1:]
+        while line != "BEGIN":
+            idx += 1;
+            line = inputStrs[idx]
+        if idx == 0:
+            alerts = []
+            inputStrs = inputStrs[1:]
+        else:
+            alerts = inputStrs[:idx]
+            inputStrs = inputStrs[idx+1:]
+    if not hasError:
 
-if not hasError:
+        # test each string against the regex
+        matches = []
+        nonMatches = []
+        for inputStr in inputStrs:
+            search = regex.fullmatch(inputStr)
+            if search:
+                matches.append(inputStr)
+            else:
+                nonMatches.append(inputStr)
+        #elapsed_time = time.process_time() - start_time
 
-  # test each string against the regex
-  matches = []
-  nonMatches = []
-  for inputStr in inputStrs:
-    search = regex.fullmatch(inputStr)
-    if search:
-        matches.append(inputStr)
-    else:
-        nonMatches.append(inputStr)
-  #elapsed_time = time.process_time() - start_time
+        # display groups if requested
+        if opts.showGroups or opts.showNamedGroups:
+            groupDict = get_group_info(regexStr, matches, opts.showNamedGroups)
+            if groupDict == None:
+                showGroups = False
+                if opts.showGroups:
+                    if hasAlert:
+                        status = status + "Regex does not have any capturing groups\n"
+                    else:
+                        hasAlert = True
+                        status = "Regex does not have any capturing groups\n"
+            else:
+                showGroups = True
+                maxLength = 7 # smallest size of format
+                for inputStr in matches:
+                    if len(inputStr) > maxLength:
+                        maxLength = len(inputStr)
+                groupFmt = "{0:" + str(maxLength) + "}  {1}"
+        else:
+            showGroups = False
 
-  # display groups if requested
-  if opts.showGroups or opts.showNamedGroups:
-      groupDict = get_group_info(regexStr, matches, opts.showNamedGroups)
-      if groupDict == None:
-          showGroups = False
-          if opts.showGroups:
-              if hasAlert:
-                  status = status + "Regex does not have any capturing groups\n"
-              else:
-                  hasAlert = True
-                  status = "Regex does not have any capturing groups\n"
-      else:
-          showGroups = True
-          maxLength = 7 # smallest size of format
-          for inputStr in matches:
-              if len(inputStr) > maxLength:
-                  maxLength = len(inputStr)
-          groupFmt = "{0:" + str(maxLength) + "}  {1}"
-  else:
-      showGroups = False
-
-  # print the stats
-  if opts.statMode:
-    fmt = "{0:30}| {1}"
-    print("--------------------------------------")
-    print(fmt.format("Matches", len(matches)))
-    print(fmt.format("Non-matches", len(nonMatches)))
-    #print(fmt.format("Time", elapsed_time))
-
-
-# create the output header
-header = "Regex: " + regexStr + "\n\n"
-if descStr != "":
-    header += ("Description: " + descStr + "\n\n")
-for line in alerts:
-    header += line
-    header += "\n"
-
-# write the output header
-if opts.outputFile:
-    outFile = open(opts.outputFile, 'w')
-    outFile.write(header)
-    if hasError:
-        outFile.close();
-        sys.exit(-1);
-else:
-    print()
-    print(header, end='')
-    if hasError:
-        sys.exit(-1);
-
-# print the match strings
-if opts.outputFile:
-    outFile.write("Matches:\n")
-else:
-    print("Matches:")
-for inputStr in sorted(matches):
-    if inputStr == "":
-        dispStr = "<empty>"
-    else:
-        dispStr = inputStr
-
-    if showGroups:
-        dispStr = groupFmt.format(dispStr, str(groupDict[inputStr]))
+        # print the stats
+        if opts.statMode:
+            fmt = "{0:30}| {1}"
+            print("--------------------------------------")
+            print(fmt.format("Matches", len(matches)))
+            print(fmt.format("Non-matches", len(nonMatches)))
+            #print(fmt.format("Time", elapsed_time))
 
     if opts.outputFile:
-        outFile.write(dispStr + "\n")
+        outFile = open(opts.outputFile, 'w')
+        if hasError:
+            outFile.close();
+            sys.exit(-1);
     else:
-        print(dispStr)
+        print()
+        print(header, end='')
+        if hasError:
+            sys.exit(-1);
 
-# print the non match strings
-if opts.outputFile:
-    outFile.write("\nNon-matches:\n")
-else:
-    print("\nNon-matches:")
-for inputStr in sorted(nonMatches):
-    if inputStr == "":
-        dispStr = "<empty>"
-    else:
-        dispStr = inputStr
-
+    # print the match strings
     if opts.outputFile:
-        outFile.write(dispStr + "\n")
-    else:
-        print(dispStr)
+        output.append({
+            "regex": regexStr,
+            "matches": matches,
+            "nonMatches": nonMatches  
+        })
 
+json.dump(output, outFile)
 # close the output
 if opts.outputFile:
     outFile.close()
