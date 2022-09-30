d49b9114db2ef98fef997de8876b30829b65e3a5
Willem Ruys <willem@founda.com>
2022-09-30 13:23:19 +0200

    modify executor program egret.py to read patterns from file

--- egret.py
+++ egret.py
@@ -2,87 +2,93 @@
 #
 # Copyright (C) 2016-2018  Eric Larson and Anna Kirk
 # elarson@seattleu.edu
-# 
+#
 # This file is part of EGRET.
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import json
 import re
-import sys	
+import sys
+from xml.etree.ElementInclude import include
 import egret_ext
 from optparse import OptionParser
 #import time
 
 # Precondition: regexStr successfully compiles and all strings in testStrings
 # match the regular expression
+
+
 def get_group_info(regexStr, testStrings, namedOnly):
-   # check for empty list
-   if len(testStrings) == 0:
-       return {}
+    # check for empty list
+    if len(testStrings) == 0:
+        return {}
 
-   # compile regex
-   regex = re.compile(regexStr)
+    # compile regex
+    regex = re.compile(regexStr)
 
-   # determine if there are named groups, numbered groups, or no groups
-   match = regex.fullmatch(testStrings[0])
-   if len(match.groupdict()) != 0:
-       useNames = True
-       names = list(match.groupdict().keys())
-       nameList = []
-       for name in names:
-           r = r"\?P<" + name
-           start = re.search(r, regexStr).start()
-           nameList.append((start, name))
-       nameList = sorted(nameList)
-       groupHdr = [ name for (start, name) in nameList ]
-   elif len(match.groups()) != 0:
-       if namedOnly:
-           return None
-       useNames = False
-   else:
-       return None
+    # determine if there are named groups, numbered groups, or no groups
+    match = regex.fullmatch(testStrings[0])
+    if len(match.groupdict()) != 0:
+        useNames = True
+        names = list(match.groupdict().keys())
+        nameList = []
+        for name in names:
+            r = r"\?P<" + name
+            start = re.search(r, regexStr).start()
+            nameList.append((start, name))
+        nameList = sorted(nameList)
+        groupHdr = [name for (start, name) in nameList]
+    elif len(match.groups()) != 0:
+        if namedOnly:
+            return None
+        useNames = False
+    else:
+        return None
 
-   # get groups for each string
-   groupDict = {}
-   for testStr in testStrings:
-       match = regex.fullmatch(testStr)
-       if useNames:
-           g = match.groupdict()
-           groupList = []
-           for i in groupHdr:
-               groupList.append({i: g[i]})
-           groupDict[testStr] = groupList
-       else:
-           groupDict[testStr] = match.groups()
+    # get groups for each string
+    groupDict = {}
+    for testStr in testStrings:
+        match = regex.fullmatch(testStr)
+        if useNames:
+            g = match.groupdict()
+            groupList = []
+            for i in groupHdr:
+                groupList.append({i: g[i]})
+            groupDict[testStr] = groupList
+        else:
+            groupDict[testStr] = match.groups()
 
-   return groupDict
+    return groupDict
+
 
 parser = OptionParser()
-parser.add_option("-f", "--file", dest = "fileName", help = "file containing regex")
-parser.add_option("-r", "--regex", dest = "regex", help = "regular expression")
-parser.add_option("-b", "--base_substring", dest = "baseSubstring",
-    default = "evil", help = "base substring for regex strings")
-parser.add_option("-o", "--output_file", dest = "outputFile", help = "output file name")
-parser.add_option("-d", "--debug", action = "store_true", dest = "debugMode",
-    default = False, help = "display debug info")
-parser.add_option("-s", "--stat", action = "store_true", dest = "statMode",
-    default = False, help = "display stats")
-parser.add_option("-g", "--groups", action = "store_true", dest = "showGroups",
-    default = False, help = "show groups")
-parser.add_option("-n", "--named_groups", action = "store_true", dest = "showNamedGroups",
-    default = False, help = "only show named groups")
+parser.add_option("-f", "--file", dest="fileName",
+                  help="file containing regex")
+parser.add_option("-r", "--regex", dest="regex", help="regular expression")
+parser.add_option("-b", "--base_substring", dest="baseSubstring",
+                  default="evil", help="base substring for regex strings")
+parser.add_option("-o", "--output_file", dest="outputFile",
+                  help="output file name")
+parser.add_option("-d", "--debug", action="store_true", dest="debugMode",
+                  default=False, help="display debug info")
+parser.add_option("-s", "--stat", action="store_true", dest="statMode",
+                  default=False, help="display stats")
+parser.add_option("-g", "--groups", action="store_true", dest="showGroups",
+                  default=False, help="show groups")
+parser.add_option("-n", "--named_groups", action="store_true", dest="showNamedGroups",
+                  default=False, help="only show named groups")
 opts, args = parser.parse_args()
 
 # check for valid command lines
@@ -101,26 +107,66 @@ if opts.fileName != None:
     for regexObject in fileAsJson:
         regexStrings.append(regexObject['pattern'])
         try:
-            descStr.append(regexObject['pattern']) 
+            descStr.append(regexObject['pattern'])
         except:
-            descStr.append("") 
+            continue
         inFile.close()
-# compile the regular expression
 
+regexStrings.remove('[A-Za-zÀ-ÖØ-öø-ÿ]\\S*')
+regexStrings.remove('[eÃ©]$')
+regexStrings.remove('([アァカヵガサザタダナハバパマヤャラワヮヷ])ー')
+regexStrings.remove('^[Â£$â‚¬?.]')
+regexStrings.remove('^[a-zA-Z0-9?!§$%#]$')
+regexStrings.remove('[ťţŧț]')
+regexStrings.remove('□[^▽]')
+regexStrings.remove('^[0-9 ]+丁目[0-9 ]+番?')
+regexStrings.remove('[\x00-\x08,\x0b-\x0c,\x0e-x1F,\x7f]')
+regexStrings.remove('((\s*<[NS]>\s*,\s*){100,})')
+regexStrings.remove('^([А-ЯЁ]{1})([а-яё]{1,15})$')
+regexStrings.remove(
+    "^(?:(?:(?:https?|ftp):)?\\/\\/)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})).?)(?::\\d{2,5})?(?:[/?#]\\S*)?$")
+
+# compile the regular expression
 for regexStr in regexStrings:
+    compileRegex = True
+    exception = ''
+    hasError = False
+    # capture regex patterns containing special characters
+    # if 'â' not in regexStr or 'Â' or regexStr or '¬' not in regexStr and '([アァカヵガサザタダナハバパマヤャラワヮヷ])ー' not in regexStr:
     try:
         regex = re.compile(regexStr)
-
+        inputStrs = [10]
         # execute regex-test
         #start_time = time.process_time()
         inputStrs = egret_ext.run(regexStr, opts.baseSubstring,
-            False, False, opts.debugMode, opts.statMode)
+                                  False, False, False, False)
         status = inputStrs[0]
         hasError = (status[0:5] == "ERROR")
+        # in this case, an error is thrown by EGRET
+        if status and status[0:5] == "ERROR":
+            output.append({'regex': regexStr, 'exceptionStackTrace': {
+                'exceptionThrownBy': 'EGRET',
+                'exception': status
+            },
+                'matches': []})
+            hasError = True
+            continue
 
+    # here we catch errors thrown by the re library
     except re.error as e:
-        status = "ERROR (compiler error): Regular expression did not compile: " + str(e)
+        # if e.msg.find('escape') != -1 or e.msg.find('unterminated subpattern') != -1 or e.msg.find('unsupported character') != -1 or e.msg.find('unknown extension') != -1:
+        #     exception = e.msg
+        #     hasError = True
+        #     output.append({'regex': regexStr, 'exception': exception,
+        #                   'matches': [], 'exceptionThrownBy': 'Python'})
+        #     continue
+        output.append({'regex': regexStr, 'exceptionStackTrace': {
+            'exceptionThrownBy': 'Python',
+            'exception':  e.msg
+        },
+            'matches': [], })
         hasError = True
+        continue
 
     if hasError:
         alerts = [status]
@@ -128,7 +174,7 @@ for regexStr in regexStrings:
         idx = 0
         line = inputStrs[idx]
         while line != "BEGIN":
-            idx += 1;
+            idx += 1
             line = inputStrs[idx]
         if idx == 0:
             alerts = []
@@ -136,69 +182,24 @@ for regexStr in regexStrings:
         else:
             alerts = inputStrs[:idx]
             inputStrs = inputStrs[idx+1:]
-    if not hasError:
+    output.append(
+        {'regex': regexStr, 'exceptionStackTrace': None, 'matches': inputStrs})
 
-        # test each string against the regex
-        matches = []
-        nonMatches = []
-        for inputStr in inputStrs:
-            search = regex.fullmatch(inputStr)
-            if search:
-                matches.append(inputStr)
-            else:
-                nonMatches.append(inputStr)
-        #elapsed_time = time.process_time() - start_time
+# open output file
+if opts.outputFile:
+    outFile = open(opts.outputFile, 'w')
+    if hasError:
+        outFile.close()
+        sys.exit(-1)
+else:
+    print()
+    print(header, end='')
+    if hasError:
+        sys.exit(-1)
 
-        # display groups if requested
-        if opts.showGroups or opts.showNamedGroups:
-            groupDict = get_group_info(regexStr, matches, opts.showNamedGroups)
-            if groupDict == None:
-                showGroups = False
-                if opts.showGroups:
-                    if hasAlert:
-                        status = status + "Regex does not have any capturing groups\n"
-                    else:
-                        hasAlert = True
-                        status = "Regex does not have any capturing groups\n"
-            else:
-                showGroups = True
-                maxLength = 7 # smallest size of format
-                for inputStr in matches:
-                    if len(inputStr) > maxLength:
-                        maxLength = len(inputStr)
-                groupFmt = "{0:" + str(maxLength) + "}  {1}"
-        else:
-            showGroups = False
-
-        # print the stats
-        if opts.statMode:
-            fmt = "{0:30}| {1}"
-            print("--------------------------------------")
-            print(fmt.format("Matches", len(matches)))
-            print(fmt.format("Non-matches", len(nonMatches)))
-            #print(fmt.format("Time", elapsed_time))
-
-    if opts.outputFile:
-        outFile = open(opts.outputFile, 'w')
-        if hasError:
-            outFile.close();
-            sys.exit(-1);
-    else:
-        print()
-        print(header, end='')
-        if hasError:
-            sys.exit(-1);
-
-    # print the match strings
-    if opts.outputFile:
-        output.append({
-            "regex": regexStr,
-            "matches": matches,
-            "nonMatches": nonMatches  
-        })
-
+# dump output to output file
 json.dump(output, outFile)
-# close the output
+
 if opts.outputFile:
     outFile.close()
 
